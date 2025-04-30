81c2ab482a820210c99a95259b57b2128f038fe9
RandGusta <gustarandiiii@gmail.com>
2025-04-30 19:35:10 -0300

    Update main.java

--- ExercicioAula07/Aresolucao/main.java
+++ ExercicioAula07/Aresolucao/main.java
@@ -1,7 +1,32 @@
 public class Main {
   public static void main(String[] args){
       Lutador l1 = new Lutador();
-    
+      l1.setNome("Lulinha calanguinho");
+      l1.setNacionalidade("Brasileiro");
+      l1.setIdade(24);
+      l1.setAltura(1.72);
+      l1.setCategoria("pena");
+      l1.setPeso(79.87);
+      l1.apresentar();
+
+      Lutador l2 = new lutador();
+      l2.setNome("Laranjinha comunista");
+      l2.setNacionalidade("Americano");
+      l2.setIdade(27);
+      l2.setAltura(1.69);
+      l2.setCategoria("pena");
+      l2.setPeso(80.87);
+      l2.apresentar();
+
+      int pontos1 = 0;
+      int pontos2 = 0;
+      if(l2.getPeso() > l1.getPeso()){
+        pontos1 += 1;
+      } else {
+        pontos22 += 2;
+      }
+      
+      
 
     
   }
