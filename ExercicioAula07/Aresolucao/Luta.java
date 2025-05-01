dacd8a903b70c2302b6ffd57ce482618cc53b616
RandGusta <gustarandiiii@gmail.com>
2025-05-01 12:45:04 -0300

    Update Luta.java

--- ExercicioAula07/Aresolucao/Luta.java
+++ ExercicioAula07/Aresolucao/Luta.java
@@ -1,6 +1,16 @@
 public class Luta{
+  private String desafiado;
+  private String desafiante;
+  private int rouds;
+  private boolean aprovado;
 
+  public void marcarLuta(){
+    
+  }
 
+  public void lutar(){
+    
+  }
 
 
   
