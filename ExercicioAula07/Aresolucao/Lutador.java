7ce4218898e41416b1c7d2cdf8ce1508ac67957e
RandGusta <gustarandiiii@gmail.com>
2025-05-01 12:36:00 -0300

    Update Lutador.java

--- ExercicioAula07/Aresolucao/Lutador.java
+++ ExercicioAula07/Aresolucao/Lutador.java
@@ -1,75 +1,92 @@
 
 public class Lutador{
-private String nome;
-private String nacionalidade;
-private int idade;
-private double altura;
-private double peso;
-private String categoria;
-private int vitorias;
-private int derrotas;
-private int empates;
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private double altura;
+    private double peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
 
-public Lutador(String nome, String nacionalidade, int idade, double altura,  double peso, int vitorias, int derrotas, int empates){
-    this.nome = nome;
-    this.nacionalidade = nacionalidade;
-    this.idade = idade;
-    this.altura = altura;
-    this.setPeso(peso);
-    this.vitorias = vitorias;
-    this.derrotas = derrotas;
-    this.empates = empates;
-}
-
-
-public void apresentar() {
-    System.out.println("Senhoras e senhores, com vocês " + this.getNome() + "!!!!!");
-    System.out.println("Sendo " + this.getNacionalidade());
-    System.out.println("Com " + this.getPeso() + " quilos ");
-    System.out.println("E " + this.getAltura() + "de altura");
-}
-
-public void status() {
-    System.out.println("Nome: " + this.nome);
-    System.out.println("Nacionalidade: " + this.nacionalidade);
-    System.out.println("Categoria: " + this.categoria);
-    System.out.println("Peso: " + this.peso);
-    System.out.println("Altura: " + this.altura);
-    System.out.println("Vitorias: " + this.vitorias);
-    System.out.println("Derrotas: " + this.derrotas);
-    System.out.println("Empates: " + this.empates);
-
-}
-
-public void ganharLuta() {
-    this.vitorias += 1;
-}
-
-public void perderLuta() {
-    this.derrotas += 1;
-}
-
-public void empatarLuta() {
-    this.empates += 1;
-}
-
-public void setPeso(double peso){
-    this.peso = peso;
-    this.categoria = definirCategoria();
-}
+    public Lutador(String nome, String nacionalidade, int idade, double altura,  double peso, int vitorias, int derrotas, int empates){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.setPeso(peso);
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
 
 
-private String definirCategoria(double peso) {
-    if(peso < 52.2){
-        return "inválido"; 
-    } else if(peso <= 70.3) {
-        return "leve";
-    } else if(peso <= 83.9){
-        return "Médio";
-    } else if(peso <= 120.2){
-        return "Pesado";
-    } else {
-        return "Inválido";
-}
+    public void apresentar() {
+        System.out.println("Senhoras e senhores, com vocês " + this.getNome() + "!!!!!");
+        System.out.println("Sendo " + this.nacionalidade);
+        System.out.println("Com " + this.getPeso() + " quilos ");
+        System.out.println("E " + this.getAltura() + "de altura");
+    }
 
-}
+    public void status() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Nacionalidade: " + this.nacionalidade);
+        System.out.println("Categoria: " + this.categoria);
+        System.out.println("Peso: " + this.peso);
+        System.out.println("Altura: " + this.altura);
+        System.out.println("Vitorias: " + this.vitorias);
+        System.out.println("Derrotas: " + this.derrotas);
+        System.out.println("Empates: " + this.empates);
+
+    }
+
+    public void ganharLuta() {
+        this.vitorias += 1;
+    }
+
+    public void perderLuta() {
+        this.derrotas += 1;
+    }
+
+    public void empatarLuta() {
+        this.empates += 1;
+    }
+
+    private void setPeso(double peso){
+        this.peso = peso;
+        this.categoria = definirCategoria(peso);
+    }
+
+
+    private String definirCategoria(double peso) {
+        if(peso < 52.2){
+            return "inválido";
+        } else if(peso <= 70.3) {
+            return "leve";
+        } else if(peso <= 83.9){
+            return "Médio";
+        } else if(peso <= 120.2){
+            return "Pesado";
+        } else {
+            return "Inválido";
+        }
+    }
+
+        public double getPeso(){
+            return this.peso;
+        }
+
+        public double getAltura(){
+            return this.altura;
+        }
+
+        public int getIdade(){
+            return this.idade;
+        }
+        public String getNome(){
+            return this.nome;
+        }
+
+
+    }
