a6247b30d41cdfd1612a6036a0ae25f8176b6d8a
RandGusta <gustarandiiii@gmail.com>
2025-05-03 17:47:18 -0300

    Update Pessoa.java

--- ExercicioAula9b/Pessoa.java
+++ ExercicioAula9b/Pessoa.java
@@ -1,18 +1,22 @@
-public class Pessoa {
-  private String nome;
-  private int idade;
-  private char sexo;
 
-  public Pessoa(String nome,int idade, char sexo){
-    this.nome = nome;
-    this.idade = idade;
-    this.sexo = sexo;
-    
-  }
-  
-  public void fazerAniversario(){
-    this.idade += 1;
-  }
+    public class Pessoa {
+        private String nome;
+        private int idade;
+        private char sexo;
 
-  
-}
+        public Pessoa(String nome, int idade, char sexo) {
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+
+        }
+
+        public void fazerAniversario() {
+            this.idade += 1;
+        }
+
+        public String toString(){
+            return this.nome;
+        }
+
+    }
