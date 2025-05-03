e85e4e97732bd2aabe2eaa275e8e821bd5599119
RandGusta <gustarandiiii@gmail.com>
2025-05-03 15:25:02 -0300

    Update Main.java

--- ExercicioAula07/Aresolucao/Main.java
+++ ExercicioAula07/Aresolucao/Main.java
@@ -1,13 +1,14 @@
 public class Main {
-  public static void main(String[] args){
-      Lutador l1 = new Lutador("Calango lulinha", "Brasileiro", 23, 1.71, 78.34, 4, 3, 1);
-      Lutador l2 = new Lutador("Laranjinha comunista", "Americano", 27, 1.69, 75.90, 3, 5, 2);
+    public static void main(String[] args){
+        Lutador l1 = new Lutador("Calango lulinha", "Brasileiro", 23, 1.71, 78.34, 4, 3, 1);
+        Lutador l2 = new Lutador("Laranjinha comunista", "Americano", 27, 1.69, 75.90, 3, 5, 2);
 
-      Luta luta1 = new Luta();
-      luta1.marcarLuta(l1, l2);
-      
+        Luta luta1 = new Luta();
+        luta1.marcarLuta(l1, l2);
+        luta1.lutar();
 
-    
-  }
-  
+
+
+    }
+
 }
