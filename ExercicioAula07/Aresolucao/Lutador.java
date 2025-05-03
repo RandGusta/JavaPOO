d614f6064ea81cb044f9128e032c650f36c7cc5f
RandGusta <gustarandiiii@gmail.com>
2025-05-03 15:24:35 -0300

    Update Lutador.java

--- ExercicioAula07/Aresolucao/Lutador.java
+++ ExercicioAula07/Aresolucao/Lutador.java
@@ -1,59 +1,100 @@
-import java.util.Random;
-public class Luta{
-    private Lutador desafiado;
-    private Lutador desafiante;
-    private int rounds;
-    private boolean aprovado;
+
+public class Lutador{
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private double altura;
+    private double peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    public Lutador(String nome, String nacionalidade, int idade, double altura,  double peso, int vitorias, int derrotas, int empates){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.setPeso(peso);
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
 
 
-    public void marcarLuta(Lutador l1, Lutador l2) {
-        if (podeLutar(l1, l2)) {
-            this.aprovado = true;
-            this.desafiante = l1;
-            this.desafiado = l2;
+    public void apresentar() {
+        System.out.println("Senhoras e senhores, com vocês " + this.getNome() + "!!!!!");
+        System.out.println("Sendo " + this.nacionalidade);
+        System.out.println("Com " + this.getPeso() + " quilos ");
+        System.out.println("E " + this.getAltura() + "de altura");
+    }
+
+    public void status() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Nacionalidade: " + this.nacionalidade);
+        System.out.println("Categoria: " + this.categoria);
+        System.out.println("Peso: " + this.peso);
+        System.out.println("Altura: " + this.altura);
+        System.out.println("Vitorias: " + this.vitorias);
+        System.out.println("Derrotas: " + this.derrotas);
+        System.out.println("Empates: " + this.empates);
+
+    }
+
+    public void ganharLuta() {
+        this.vitorias += 1;
+    }
+
+    public void perderLuta() {
+        this.derrotas += 1;
+    }
+
+    public void empatarLuta() {
+        this.empates += 1;
+    }
+
+    private void setPeso(double peso){
+        this.peso = peso;
+        this.categoria = definirCategoria(peso);
+    }
+
+
+    private String definirCategoria(double peso) {
+        if(peso < 52.2){
+            return "inválido";
+        } else if(peso <= 70.3) {
+            return "leve";
+        } else if(peso <= 83.9){
+            return "Médio";
+        } else if(peso <= 120.2){
+            return "Pesado";
         } else {
-            this.aprovado = false;
-            this.desafiante = null;
-            this.desafiado = null;
-        }
-    }
-        private boolean podeLutar(Lutador l1, Lutador l2){
-            if(l1.getCategoria().equals(l2.getCategoria()) && (l1 != l2)){
-                return true;
-            } else {
-                return false;
-    }
-    }
-
-    private void apresentarLutadores(){
-        if(this.aprovado){
-            this.desafiante.apresentar();
-            this.desafiado.apresentar();
+            return "Inválido";
         }
     }
 
-    public void Lutar(boolean aprovado){
-        if(aprovado){
-            apresentarLutadores();
-            Random random = new Random();
-            int vencedor = random.nextInt(2);
-            definirVencedor(vencedor);
-        } else {
-            System.out.println("A luta não foi aprovada!!");
+        public double getPeso(){
+            return this.peso;
         }
+
+        public double getAltura(){
+            return this.altura;
+        }
+
+        public int getIdade(){
+            return this.idade;
+        }
+        public String getNome(){
+            return this.nome;
+        }
+
+        
+        public String getCategoria(){
+        return this.categoria;
+        }
+
+        public String toString() {
+        return this.nome;
     }
 
-        private void definirVencedor(int numero){
-            if(numero == 1){
-                System.out.println("Palmas para o vencedor "+ this.desafiante);
-            } else if (numero ==2 ){
-                System.out.println("Palmas para o vencedor " + this.desafiado);
-            } else {
-                System.out.println("Empate senhoras e senhores!!");
-            }
-
-        }
-}
-
-
-
+    }
