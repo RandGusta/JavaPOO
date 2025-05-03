2abf06c6108e269e848031ead1ff4ba3a5992d5d
RandGusta <gustarandiiii@gmail.com>
2025-05-03 17:48:05 -0300

    Update Livro.java

--- ExercicioAula9b/Livro.java
+++ ExercicioAula9b/Livro.java
@@ -1,56 +1,58 @@
+
 public class Livro implements Publicacao{
-  private String titulo;
-  private String autor;
-  private int totPag;
-  private int pagAtual;
-  private boolean aberto;
-  private Pessoa leitor;
+    private String titulo;
+    private String autor;
+    private int totPag;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
 
-  public Livro(String titulo, String autor, int totPag, Pessoa leitor){
-    this.titulo = titulo;
-    this.autor = autor;
-    this.totPag = totPag;
-    this.pagAtual = 0;
-    this.aberto = false;
-    this.leitor = leitor;
-  }
+    public Livro(String titulo, String autor, int totPag, Pessoa leitor){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPag = totPag;
+        this.pagAtual = 0;
+        this.aberto = false;
+        this.leitor = leitor;
+    }
 
 
-  public void detalhes(){
-    System.out.println("titulo: " + this.titulo);
-    System.out.println("Autor: " + this.autor);
-    System.out.println("Total de páginas: " + this.totPag);
-    System.out.println("Emprestado: " + this.emprestado);
-  }
+    public void detalhes(){
+        System.out.println("titulo: " + this.titulo);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Total de páginas: " + this.totPag);
+        System.out.println("Leitor: " + this.leitor);
+        System.out.println("Página atual: " + this.pagAtual);
+    }
 
-  
-  @Override
-  public void abrir(){
-    this.aberto = true;
-  }
-  
-  @Override
-  public void fechar(){
-    this.aberto = false;
-  }
-  
-  @Override 
-  public void folhear(int pag){
-    this.abrir();
-    this.pagAtual = pag;
-  }
-  
-  @Override 
-  public void avancarPagina(){
-    this.abrir();
-    this.pagAtual += 1;
-  }
-  
-  @Override
-  public void voltarPagina(){
-    this.abrir();
-    this.pagAtual -= 1;
-  }
-  
-  
+
+    @Override
+    public void abrir(){
+        this.aberto = true;
+    }
+
+    @Override
+    public void fechar(){
+        this.aberto = false;
+    }
+
+    @Override
+    public void folhear(int pag){
+        this.abrir();
+        this.pagAtual = pag;
+    }
+
+    @Override
+    public void avancarPagina(){
+        this.abrir();
+        this.pagAtual += 1;
+    }
+
+    @Override
+    public void voltarPagina(){
+        this.abrir();
+        this.pagAtual -= 1;
+    }
+
+
 }
