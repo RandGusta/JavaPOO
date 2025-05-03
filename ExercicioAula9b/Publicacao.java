ea4e270f3fa6b9f02f8945dfa94e908c02078c97
RandGusta <gustarandiiii@gmail.com>
2025-05-03 17:02:06 -0300

    Update Publicacao.java

--- ExercicioAula9b/Publicacao.java
+++ ExercicioAula9b/Publicacao.java
@@ -1,7 +1,9 @@
 public interface Publicacao{
 
-  public void 
+  public void abrir(){}
+  public void fechar(){}
+  public void folhear(int pag){}
+  public void avancarPagina(){}
+  public void voltarPagina(){}
 
-
-  
 }
