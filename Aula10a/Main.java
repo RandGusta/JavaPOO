77ceba2b27270341a4597fd790c1b0d90db3fd3f
RandGusta <gustarandiiii@gmail.com>
2025-05-07 13:46:19 -0300

    Update Main.java

--- Aula10a/Main.java
+++ Aula10a/Main.java
@@ -4,6 +4,18 @@ public class Main{
     Aluno p2 = new Aluno();
     Professor p3 = new Professor();
     Funcionario p4 = new Funcionario();
+
+    p1.setNome("Maria");
+    p2.setNome("João");
+    p3.setNome("Claudia");
+    p4.setNome("Gabriel");
+
+
+    p2.setCurso("Informática");
+    p3.setSalario(2500.75);
+    p4.setSetor("Estoque");
     
+  
+  
   }
 }
