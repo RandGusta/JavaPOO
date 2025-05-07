6373a135069f324a8e7f51c16c29e827f816199e
RandGusta <gustarandiiii@gmail.com>
2025-05-07 11:28:03 -0300

    Create Funcionario.java

---
+++ Aula10a/Funcionario.java
@@ -0,0 +1,15 @@
+public class Funcionario extends Pessoa{
+  private String setor;
+  private boolean trabalhando;
+
+
+  public void mudarTrabalho(){
+  }
+
+  public String getSetor(){
+    return this.setor;
+  }
+  public boolean getTrabalhando(){
+    return this.trabalhando;
+  }
+}
